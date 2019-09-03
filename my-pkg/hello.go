8fcb0aed8fd29f8c330a407e253192b94a924333
Javier Carmona <jcarm010@fiu.edu>
2019-09-03 10:17:37 -0400

    added travis and coveralls

---
+++ my-pkg/hello.go
@@ -0,0 +1,7 @@
+package my_pkg
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Hello World")
+}
---
+++ my-pkg/hello_test.go
@@ -0,0 +1,7 @@
+package my_pkg
+
+import "testing"
+
+func TestSayHello(t *testing.T) {
+	SayHello()
+}
